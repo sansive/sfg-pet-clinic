771fa352846ac7b85648686bd93242c34bf8ab75
Sandra Sierra <sansive00@gmail.com>
2022-09-19 13:24:48 +0200

    Implement Interfaces for services. Closes #5

---
+++ pet-clinic-data/src/main/java/sansive/springframework/sfgpetclinic/services/PetService.java
@@ -0,0 +1,11 @@
+package sansive.springframework.sfgpetclinic.services;
+
+import sansive.springframework.sfgpetclinic.model.Pet;
+
+import java.util.Set;
+
+public interface PetService {
+    Pet findById(Long id);
+    Pet save(Pet pet);
+    Set<Pet> findAll();
+}
---
+++ pet-clinic-data/src/main/java/sansive/springframework/sfgpetclinic/services/VetService.java
@@ -0,0 +1,11 @@
+package sansive.springframework.sfgpetclinic.services;
+
+import sansive.springframework.sfgpetclinic.model.Vet;
+
+import java.util.Set;
+
+public interface VetService {
+    Vet findById(Long id);
+    Vet save(Vet vet);
+    Set<Vet> findAll();
+}
