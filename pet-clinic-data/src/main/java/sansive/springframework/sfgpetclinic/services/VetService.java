ebc642124d77b7d57951429760d2bae62ffd74f0
Sandra Sierra <sansive00@gmail.com>
2022-09-20 10:34:03 +0200

    Refactoring to common crud interface. Closes #11

--- pet-clinic-data/src/main/java/sansive/springframework/sfgpetclinic/services/VetService.java
+++ pet-clinic-data/src/main/java/sansive/springframework/sfgpetclinic/services/VetService.java
@@ -4,8 +4,6 @@ import sansive.springframework.sfgpetclinic.model.Vet;
 
 import java.util.Set;
 
-public interface VetService {
-    Vet findById(Long id);
-    Vet save(Vet vet);
-    Set<Vet> findAll();
+public interface VetService extends CrudService<Vet, Long> {
+
 }
