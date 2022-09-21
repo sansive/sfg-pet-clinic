737a6e8050f0fb788efec9e14814993591469b08
Sandra Sierra <sansive00@gmail.com>
2022-09-21 12:25:42 +0200

    Adding bootstrap for data. Closes #14

--- pet-clinic-data/src/main/java/sansive/springframework/sfgpetclinic/services/map/PetServiceMap.java
+++ pet-clinic-data/src/main/java/sansive/springframework/sfgpetclinic/services/map/PetServiceMap.java
@@ -1,11 +1,11 @@
 package sansive.springframework.sfgpetclinic.services.map;
 
 import sansive.springframework.sfgpetclinic.model.Pet;
-import sansive.springframework.sfgpetclinic.services.CrudService;
+import sansive.springframework.sfgpetclinic.services.PetService;
 
 import java.util.Set;
 
-public class PetServiceMap extends AbstractMapService<Pet, Long> implements CrudService<Pet, Long> {
+public class PetServiceMap extends AbstractMapService<Pet, Long> implements PetService {
     @Override
     public Set<Pet> findAll() {
         return super.findAll();
--- pet-clinic-data/src/main/java/sansive/springframework/sfgpetclinic/services/map/VetServiceMap.java
+++ pet-clinic-data/src/main/java/sansive/springframework/sfgpetclinic/services/map/VetServiceMap.java
@@ -1,11 +1,11 @@
 package sansive.springframework.sfgpetclinic.services.map;
 
 import sansive.springframework.sfgpetclinic.model.Vet;
-import sansive.springframework.sfgpetclinic.services.CrudService;
+import sansive.springframework.sfgpetclinic.services.VetService;
 
 import java.util.Set;
 
-public class VetServiceMap extends AbstractMapService<Vet, Long> implements CrudService<Vet, Long> {
+public class VetServiceMap extends AbstractMapService<Vet, Long> implements VetService {
 
     @Override
     public Set<Vet> findAll() {
